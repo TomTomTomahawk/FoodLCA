90b74690a85509c0e2db5c977dab9f9025555b66
TomTomTomahawk <52259295+TomTomTomahawk@users.noreply.github.com>
2020-06-01 00:51:02 +0200

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,15 @@
-
-
 import 'package:chart_tuto/navigation_bar_controller.dart';
+import 'package:chart_tuto/views/library_list.dart';
 import 'package:flutter/material.dart';
-
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() => runApp(MyApp());
 
-
-
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: BottomNavigationBarController(),
+      home: LibraryList(),
     );
   }
 }
