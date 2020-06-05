cbc40b6369e21fa45f210dcae2460eccaf84d2dd
TomTomTomahawk <52259295+TomTomTomahawk@users.noreply.github.com>
2020-06-05 05:59:12 +0200

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:chart_tuto/navigation_bar_controller.dart';
 import 'package:chart_tuto/views/library_list.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() => runApp(MyApp());
 
