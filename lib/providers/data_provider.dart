e611c3a8668eb79c7ee90ad7ab2153a1b7c491d9
Thomas Le Varlet <52259295+TomTomTomahawk@users.noreply.github.com>
2020-08-08 13:55:05 +0200

    Add files via upload

--- lib/providers/data_provider.dart
+++ lib/providers/data_provider.dart
@@ -6,13 +6,13 @@ class DataProvider {
 
   static Future _onConfigure(Database db) async {
     await db.execute('PRAGMA foreign_keys = ON');
-}
+  }
 
   static Future open() async {
     db = await openDatabase(join(await getDatabasesPath(), 'database23.db'),
-        version: 1, onConfigure: _onConfigure, onCreate: (Database db, int version) async {
-
-        await db.execute('''
+        version: 1,
+        onConfigure: _onConfigure, onCreate: (Database db, int version) async {
+      await db.execute('''
             CREATE TABLE recipes(
              id integer constraint recipes_pk primary key autoincrement,
              name  varchar(50) not null,
@@ -20,7 +20,7 @@ class DataProvider {
            );
          ''');
 
-         await db.execute('''
+      await db.execute('''
            CREATE TABLE ingredients(
              id integer not null constraint ingredients_pk primary key autoincrement,
              name  varchar(50),
@@ -42,7 +42,7 @@ class DataProvider {
     }
     //return await db.query('Ingredients'); WHERE draft = TRUE'''
     return await db.rawQuery('''SELECT * FROM Ingredients
-    WHERE draft =?''',[1]);
+    WHERE draft =?''', [1]);
   }
 
   static Future insertIngredient(Map<String, dynamic> ingredient) async {
@@ -50,15 +50,13 @@ class DataProvider {
   }
 
   static Future updateIngredient(Map<String, dynamic> ingredient) async {
-    await db.update('Ingredients', ingredient, where: 'id = ?', whereArgs: [ingredient['id']]);
+    await db.update('Ingredients', ingredient,
+        where: 'id = ?', whereArgs: [ingredient['id']]);
   }
 
   static Future deleteIngredient(int id) async {
     await db.delete('Ingredients', where: 'id = ?', whereArgs: [id]);
   }
-
-
-
 
   static Future insertRecipe(Map<String, dynamic> recipe) async {
     await db.insert('Recipes', recipe);
@@ -71,59 +69,72 @@ class DataProvider {
     return await db.query('Recipes');
   }
 
-  static Future<List<Map<String, dynamic>>> getRecipeIngredientsList(recipeid) async {
+  static Future<List<Map<String, dynamic>>> getRecipeIngredientsList(
+      recipeid) async {
     if (db == null) {
       await open();
     }
     //return await db.query('Ingredients'); WHERE draft = TRUE'''
     return await db.rawQuery('''SELECT * FROM Ingredients
-    WHERE recipe_id =?''',[recipeid]);
+    WHERE recipe_id =?''', [recipeid]);
   }
+
 //SELECT EXISTS(SELECT * FROM recipes WHERE draft = TRUE)
   static Future getBuildMode() async {
     if (db == null) {
-       await open();
+      await open();
     }
-     //var result = await db.rawQuery('''SELECT SUM(id) FROM recipes;''');
-    var result = await db.rawQuery('''SELECT EXISTS(SELECT * FROM recipes WHERE draft = TRUE);''');
+    //var result = await db.rawQuery('''SELECT SUM(id) FROM recipes;''');
+    var result = await db.rawQuery(
+        '''SELECT EXISTS(SELECT * FROM recipes WHERE draft = TRUE);''');
     return result[0]["EXISTS(SELECT * FROM recipes WHERE draft = TRUE)"];
   }
 
-    static Future getRecipeMax() async {
+  static Future getRecipeMax() async {
     if (db == null) {
-       await open();
+      await open();
     }
-     //var result = await db.rawQuery('''SELECT SUM(id) FROM recipes;''');
+    //var result = await db.rawQuery('''SELECT SUM(id) FROM recipes;''');
     var result = await db.rawQuery('''SELECT MAX(id) FROM recipes;''');
     return result[0]["MAX(id)"];
   }
 
-    static Future deleteRecipe(int id) async {
+  static Future deleteRecipe(int id) async {
     await db.delete('Recipes', where: 'id = ?', whereArgs: [id]);
   }
 
-    static Future<List<Map<String, dynamic>>> getRecipeNameFromId(recipeid) async {
+  static Future<List<Map<String, dynamic>>> getRecipeNameFromId(
+      recipeid) async {
     if (db == null) {
       await open();
     }
     //return await db.query('Ingredients'); WHERE draft = TRUE'''
-    var result= await db.rawQuery('''SELECT * FROM recipes
-    WHERE id =?''',[recipeid]);
+    var result = await db.rawQuery('''SELECT * FROM recipes
+    WHERE id =?''', [recipeid]);
     return result[0]['name'];
   }
 
-  static Future<List<Map<String, dynamic>>> getCompareRecipeIngredientsList(recipeid,comparerecipeid) async {
+  static Future<List<Map<String, dynamic>>> getCompareRecipeIngredientsList(
+      recipeid, comparerecipeid) async {
     if (db == null) {
       await open();
     }
     //return await db.query('Ingredients'); WHERE draft = TRUE'''
     return await db.rawQuery('''SELECT * FROM Ingredients
     WHERE recipe_id =?
-    OR recipe_id =?''',[recipeid,comparerecipeid]);
+    OR recipe_id =?''', [recipeid, comparerecipeid]);
   }
 
   static Future updateRecipe(Map<String, dynamic> recipe) async {
-    await db.update('recipes', recipe, where: 'id = ?', whereArgs: [recipe['id']]);
+    await db
+        .update('recipes', recipe, where: 'id = ?', whereArgs: [recipe['id']]);
   }
 
+  static Future<List<Map<String, dynamic>>> getAllIngredients() async {
+    if (db == null) {
+      await open();
+    }
+    //return await db.query('Ingredients'); WHERE draft = TRUE'''
+    return await db.rawQuery('''SELECT * FROM ingredients''');
+  }
 }
